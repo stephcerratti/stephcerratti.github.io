5aea8f4bd662776cbe82bf44901f67ef10e087fe
stephcerratti <stephanie.cerratti@gmail.com>
2017-07-18 12:53:04 -0400

    #11 GitHub pages change

---
+++ main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+$('.owl-carousel').owlCarousel({
+    // rtl:true,
+    loop:false,
+    margin:10,
+    // nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1024:{
+            items:3
+        }
+    }
+});
+
+$(function(){
+    $(".flip").flip({
+        trigger: 'click'
+    });
+});
+
+})
